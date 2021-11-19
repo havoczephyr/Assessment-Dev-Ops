8088d4821a2faa67e90c6a1be61538ee05dd12bb
Havoczephyr <havoczephyr@hotmail.com>
2021-11-19 18:32:59 -0500

    post completion css fix (changed /css to /styles)

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
     rollbar.info('player has connected, and is ready to play!')
 })
 
-app.use('/css', express.static(path.join(__dirname, './public/index.css')))
+app.use('/styles', express.static(path.join(__dirname, './public/index.css')))
 app.use('/js', express.static(path.join (__dirname, './public/index.js')))
 
 app.get('/api/robots', (req, res) => {
